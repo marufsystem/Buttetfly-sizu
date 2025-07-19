068a00b30e562c442df1aaab5c00841af3b9a1cb
marufsystem <tuui3578@gmail.com>
2025-07-19 23:38:59 +0600

    Create logger.js

---
+++ utils/logger.js
@@ -0,0 +1,28 @@
+const chalk = require("chalk");
+const moment = require("moment-timezone");
+
+function log(content, type = "INFO") {
+  const time = moment().tz("Asia/Dhaka").format("HH:mm:ss");
+  switch (type) {
+    case "INFO":
+      console.log(`${chalk.green.bold("[ INFO ]")} ${chalk.gray(`[${time}]`)} › ${content}`);
+      break;
+    case "WARN":
+      console.log(`${chalk.yellow.bold("[ WARN ]")} ${chalk.gray(`[${time}]`)} › ${content}`);
+      break;
+    case "ERROR":
+      console.log(`${chalk.red.bold("[ ERROR ]")} ${chalk.gray(`[${time}]`)} › ${content}`);
+      break;
+    case "READY":
+      console.log(`${chalk.cyan.bold("[ READY ]")} ${chalk.gray(`[${time}]`)} › ${content}`);
+      break;
+    case "STARTING":
+      console.log(`${chalk.blue.bold("[ START ]")} ${chalk.gray(`[${time}]`)} › ${content}`);
+      break;
+    default:
+      console.log(`${chalk.white.bold("[ LOG ]")} ${chalk.gray(`[${time}]`)} › ${content}`);
+      break;
+  }
+}
+
+module.exports = log;
